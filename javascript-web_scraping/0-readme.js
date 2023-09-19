a4096d5d1121b9e42ea3ce771931bb1d1ea0e928
Angella Lao <6369@holbertonstudents.com>
2023-09-19 06:23:52 +0000

    script that reads and prints the content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const args = process.argv[2]
+fs.readFile(args, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
