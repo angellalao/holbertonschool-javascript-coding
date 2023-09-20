b5c318f6bf77aa4b9fa65a1553fe9a6ee7b19dd7
Angella Lao <6369@holbertonstudents.com>
2023-09-20 05:03:49 +0000

    edit

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const content = process.argv[3];
 const file = process.argv[2];
-fs.writeFile(file, content, err => {
+fs.writeFile(file, content, (err) => {
   if (err) {
     console.error(err);
   }
