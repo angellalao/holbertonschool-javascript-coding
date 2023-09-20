72090fbdb0754ae37ffea44915418cc45cc62647
Angella Lao <6369@holbertonstudents.com>
2023-09-20 05:43:25 +0000

    filter through inputed api, return count

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+const charUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+const requestSettings = {
+  method: 'GET',
+  url: `${process.argv[2]}`
+};
+
+request(requestSettings, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
+    const movieData = JSON.parse(body).results;
+    let count = 0;
+    for (const movie of movieData) {
+      //console.log(movie.characters);
+      for (const char of movie.characters) {
+        if (char === charUrl) {
+          count = count + 1;
+        }
+      }
+    }
+    console.log(count);
+    //console.log(movieData);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
+});
