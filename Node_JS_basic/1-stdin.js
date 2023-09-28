739cf3a196280193f314227ad00df76b53f0fe74
Angella Lao <6369@holbertonstudents.com>
2023-09-29 01:21:18 +1000

    add encoding utf8

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  encoding: 'utf8',
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
