79f7f8abb5b725bebaf6eeb3a8a15f8276223093
Angella Lao <6369@holbertonstudents.com>
2023-10-14 11:27:36 +1100

    create a more complex http server using express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const studentData = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${studentData}`);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
